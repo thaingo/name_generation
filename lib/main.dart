66674f2f4ea6ec66b0487e6daa8fa2d33a8edaee
thaingo <baothaingo@gmail.com>
2018-10-29 22:28:59 +0700

    generate a pairWord for a startup name

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main()=> runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+  final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to ABC',
       home: Scaffold(
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to ABC'),
         ),
         body: Center(
-          child: Text('Hello world'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
